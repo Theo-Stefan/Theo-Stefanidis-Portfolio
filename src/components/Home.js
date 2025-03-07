cbcb040c0a6de17263eb3e193a4d55f35b77f4f0
Theodoros Stefanidis <stefteo10@gmail.com>
2025-03-07 22:28:59 +0200

    Home
    added text and button

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,20 @@ const Home = () => {
       <div className="triangle one"></div>
       <div className="triangle two"></div>
 
+      <div className="message">
+        <p>Hey there! I am</p>
+        <h1>Theo</h1>
+        <h1>Stefanidis</h1>
+      </div>
+
+      <div className="profession">
+        <h1>Software</h1>
+        <h1>Developer</h1>
+      </div>
+
       <img className="photo" src={myPhoto} alt="Theo" />
+
+      <button className="resume-btn">My Resume</button>
     </section>
   );
 };

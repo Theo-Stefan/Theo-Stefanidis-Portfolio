15560e3ff21c7e7e00e6787c764680cf5dcfa46e
Theodoros Stefanidis <stefteo10@gmail.com>
2025-03-25 17:14:30 +0200

    Dark Theme button
    implemented light/dark theme switch. It works perfectly!

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/Header.css';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const topOffset = 80; //when smooth scrolling it needs a little bit of offset
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const topOffset = 80; // Offset for smooth scrolling
 
   useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 0);
-    };
+    const handleScroll = () => setIsScrolled(window.scrollY > 0);
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  // Smooth Scroll Function
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia(
+      '(prefers-color-scheme: dark)',
+    ).matches;
+    const currentTheme = prefersDarkMode ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    setIsDarkTheme(prefersDarkMode);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = isDarkTheme ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   const scrollToSection = (id) => {
     const section = document.getElementById(id);
     if (section) {
@@ -35,34 +50,44 @@ const Header = () => {
 
   return (
     <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
-      <div className="logo">
-        <a onClick={refreshToHome} className="logo-link">
-          Theo.
-        </a>
+      <div className="header-container">
+        <div className="logo">
+          <a onClick={refreshToHome} className="logo-link">
+            Theo.
+          </a>
+        </div>
+
+        <nav className="nav">
+          <ul>
+            <li>
+              <a onClick={() => scrollToSection('home')}>Home</a>
+            </li>
+            <li>
+              <a onClick={() => scrollToSection('about')}>About</a>
+            </li>
+            <li>
+              <a onClick={() => scrollToSection('career')}>Career Journey</a>
+            </li>
+            <li>
+              <a onClick={() => scrollToSection('projects')}>Projects</a>
+            </li>
+          </ul>
+        </nav>
+
+        <button
+          className="contact-btn"
+          onClick={() => scrollToSection('contact')}
+        >
+          Contact
+        </button>
       </div>
 
-      <nav className="nav">
-        <ul>
-          <li>
-            <a onClick={() => scrollToSection('home')}>Home</a>
-          </li>
-          <li>
-            <a onClick={() => scrollToSection('about')}>About</a>
-          </li>
-          <li>
-            <a onClick={() => scrollToSection('career')}>Career Journey</a>
-          </li>
-          <li>
-            <a onClick={() => scrollToSection('projects')}>Projects</a>
-          </li>
-        </ul>
-      </nav>
-
-      <button
-        className="contact-btn"
-        onClick={() => scrollToSection('contact')}
-      >
-        Contact
+      <button id="theme-switch-btn" onClick={toggleTheme}>
+        {isDarkTheme ? (
+          <FontAwesomeIcon icon={faSun} />
+        ) : (
+          <FontAwesomeIcon icon={faMoon} />
+        )}
       </button>
     </header>
   );

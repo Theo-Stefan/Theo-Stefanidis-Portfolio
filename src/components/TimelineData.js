96aabeb85219961dae7cc0daf2fa90da2b50e76d
Theodoros Stefanidis <stefteo10@gmail.com>
2025-03-26 10:45:40 +0200

    Update Timeline Data

--- src/components/TimelineData.js
+++ src/components/TimelineData.js
@@ -3,19 +3,17 @@ import {
   faBriefcase,
   faPersonMilitaryRifle,
   faBookOpen,
+  faSchool,
 } from '@fortawesome/free-solid-svg-icons';
 
 export const TimelineData = [
   {
     id: 1,
-    icon: faGraduationCap,
+    icon: faSchool,
     date: '2016 - 2019',
-    title: 'Graduated from highschool',
+    title: 'High School Graduation',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ' +
-      'Nunc condimentum lacus dolor, quis dictum dolor consequat non. ' +
-      'Duis vel urna eget nulla commodo lobortis. Aenean dictum sed ex at blandit.',
+      'I completed my high school education in Thessaloniki, Greece, graduating with a mark of 18.4/20 (excellent) — a result that reflected my dedication and hard work throughout those years.',
   },
   {
     id: 2,
@@ -23,32 +21,23 @@ export const TimelineData = [
     date: '01/10/2019',
     title: 'University - Computer Science Department',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ' +
-      'Nunc condimentum lacus dolor, quis dictum dolor consequat non. ' +
-      'Duis vel urna eget nulla commodo lobortis. Aenean dictum sed ex at blandit.',
+      'I was admitted to the Computer Science Department at the Aristotle University of Thessaloniki, embarking on a journey that deepened my understanding of technology and problem-solving.',
   },
   {
     id: 3,
     icon: faBriefcase,
     date: '01/03/2024 - 01/06/2024',
-    title: 'Intership job in Milan, Italy',
+    title: 'Intership Experience at Value+ in Milan, Italy',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ' +
-      'Nunc condimentum lacus dolor, quis dictum dolor consequat non. ' +
-      'Duis vel urna eget nulla commodo lobortis. Aenean dictum sed ex at blandit.',
+      'I completed a 3-month internship at Value+ in Milan, Italy, where I gained hands-on experience in React JavaScript and web development. I also improved my Italian and collaborated effectively with my colleagues, enhancing both my technical and interpersonal skills.\n(https://www.valueplus.it/)',
   },
   {
     id: 4,
     icon: faGraduationCap,
     date: '04/12/2024',
-    title: 'Graduated from University',
+    title: 'University Graduation',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ' +
-      'Nunc condimentum lacus dolor, quis dictum dolor consequat non. ' +
-      'Duis vel urna eget nulla commodo lobortis. Aenean dictum sed ex at blandit.',
+      'I graduated from the Computer Science Department at Aristotle University of Thessaloniki, where I explored multiple areas, from programming and systems architecture to databases and software engineering. Working on a wide range of projects helped me develop a versatile skill set that I continue to build on in my professional journey.',
   },
   {
     id: 5,
@@ -56,9 +45,6 @@ export const TimelineData = [
     date: '15/11/2024 - 15/05/2025',
     title: 'Military Service',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ' +
-      'Nunc condimentum lacus dolor, quis dictum dolor consequat non. ' +
-      'Duis vel urna eget nulla commodo lobortis. Aenean dictum sed ex at blandit.',
+      "In compliance with Greece's mandatory military service, I took a six-month break from my career to serve in the armed forces.",
   },
 ];

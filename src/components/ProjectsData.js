8decfd4ce2930440f23f48a4f4984fcae6beeedc
Theodoros Stefanidis <stefteo10@gmail.com>
2025-03-29 11:47:20 +0200

    Update ProjectsData.js

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -22,8 +22,7 @@ export const ProjectsData = [
     id: 1,
     title: 'My First Video Game - Maze Rush',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ',
+      'At the age of 14, I created my very first video game using Game Maker Studio — an experience that sparked my passion for programming. In this game, the player controls a smiley face navigating through intricate mazes filled with enemies and obstacles, aiming to reach the portal that leads to the next level. This project marked my first step into the world of game development and interactive experiences.',
     thumbnail: MazeRush2,
     images: [MazeRush1, MazeRush2, MazeRush3, MazeRush4, MazeRush5, MazeRush6],
     technologies: 'GameMaker Language (GML)',
@@ -35,8 +34,7 @@ export const ProjectsData = [
     id: 2,
     title: 'The Number (Video Game)',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ',
+      'At 15, I developed The Number, a mind-reading game built with Game Maker Studio. The player thinks of a number between 1 and 31, and through a series of clever questions, the game successfully guesses it.',
     thumbnail: Number5,
     images: [Number1, Number2, Number3, Number4, Number5, Number6],
     technologies: 'GameMaker Language (GML)',
@@ -48,8 +46,7 @@ export const ProjectsData = [
     id: 3,
     title: 'Run 4 Life (Video Game)',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ',
+      'Run 4 Life is a 3D action game I developed as part of a university project. In this fast-paced challenge, the player must dodge obstacles, collect diamonds, and reach the finish line. It was my first experience combining 3D graphics, sound effects, and animations, all brought to life using Unity and C#.',
     thumbnail: Run4Life3,
     images: [Run4Life1, Run4Life2, Run4Life3, Run4Life4],
     technologies: 'C#',
@@ -61,12 +58,22 @@ export const ProjectsData = [
     id: 4,
     title: 'FortKnight',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ' +
-      'pellentesque vulputate. Sed fringilla odio interdum efficitur viverra. ',
+      "As a chess enthusiast, I contributed to FortKnight, a web-based chess game with unique rules. I added a toggle switch that visualizes the knight's possible moves using green dots, improving clarity for players. This contribution challenged me to dive into an unfamiliar codebase written in HTML, CSS, and JavaScript, enhancing both my coding skills and my ability to adapt to new projects.",
     thumbnail: fortknight1,
     images: [fortknight2],
     technologies: 'HTML, CSS, JavaScript',
     category: ['Game Development', 'Web Development'],
+    projectLink: 'https://acenturyandabit.github.io/fortknight',
+  },
+  {
+    id: 5,
+    title: 'Viva Customers',
+    description:
+      'To manage my 3D printing business, I developed Viva Customers, a Java Swing application that stores customer details and sales history in an SQL database. This project helped me learn how to build a responsive UI and implement efficient database handling, ensuring a lightweight program that runs smoothly on any Windows computer.',
+    thumbnail: fortknight1,
+    images: [fortknight2],
+    technologies: 'Java Swing, SQL',
+    category: ['Desktop Applications'],
     projectLink: 'https://acenturyandabit.github.io/fortknight',
   },
 ];

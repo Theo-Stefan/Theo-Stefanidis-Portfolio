0e0dfaa8df5b5932c47092d60d3ad51dd4adbbef
Theodoros Stefanidis <stefteo10@gmail.com>
2025-05-03 13:52:16 +0300

    Mobile Home fix
    fix global screen width
    fix mobile home section
    fix a little header mobile section
    Header and Home ready, move on to the "About" section!

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import '../styles/Home.css';
 import myPhoto from '../assets/MyPhoto/MyPhoto.png';
 
 const Home = () => {
   const { t } = useTranslation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 830);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 830);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <section id="home" className="home">
@@ -13,8 +20,17 @@ const Home = () => {
 
       <div className="message">
         <p>{t('home.hey')}</p>
-        <h1>{t('home.name')}</h1>
-        <h1>{t('home.surname')}</h1>
+        {!isMobile ? (
+          <>
+            <h1>{t('home.name')}</h1>
+            <h1>{t('home.surname')}</h1>
+          </>
+        ) : (
+          <div className="message-names">
+            <h1>{t('home.name')}</h1>
+            <h1>{t('home.surname')}</h1>
+          </div>
+        )}
       </div>
 
       <div className="profession">
